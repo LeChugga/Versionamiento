585abbe8873b127821b8c07588ba5712789d80ad
LeChugga <chuggamunoz@gmail.com>
2023-02-14 08:28:08 -0600

    Version 1.0.0

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Ámate a ti mismo como si tu vida dependiera de ello", tipo: "buzo", cantidad: 1, desc: "Libro", precio: 1200, talle: "L", img: './img/amate.jpg'},
+    {id: 2, nombre: "Tus zonas erróneas", tipo: "buzo", cantidad: 1, desc: "Libro", precio: 1100, talle: "L", img: './img/tus.jpg'},
+    {id: 3, nombre: "Sal de tu mente, entra en tu vida", tipo: "buzo", cantidad: 1, desc: "Libro", precio: 1200, talle: "M", img: './img/sal.jpg'},
+    {id: 4, nombre: "Transforma tu cerebro con PNL", tipo: "buzo", cantidad: 1, desc: "Libro", precio: 1400, talle: "M", img: './img/transforma.jpg'},
+    {id: 5, nombre: "Inteligencia emocional 2.0", tipo: "buzo", cantidad: 1, desc: "Libro", precio: 1200, talle: "S", img: './img/inteligencia.jpg'},
+    {id: 6, nombre: "Cómo superarse en tiempos de crisis", tipo: "buzo", cantidad: 1, desc: "Libro", precio: 1500, talle: "S", img: './img/superarse.jpg'},
+    {id: 7, nombre: "Los dones de la imperfección", tipo: "remera", cantidad: 1, desc: "Libro", precio: 500, talle: "L", img: './img/dones.jpg'},
+    {id: 8, nombre: "El alquimista", tipo: "remera", cantidad: 1, desc: "Libro", precio: 500, talle: "L", img: './img/alquimista.jpg'},
+    {id: 9, nombre: "El caballero de la armadura tóxica", tipo: "remera", cantidad: 1, desc: "Libro", precio: 500, talle: "M", img: './img/caballero.jpg'},
+    {id: 10, nombre: "El monje que vendió su Ferrari", tipo: "remera", cantidad: 1, desc: "Libro", precio: 700, talle: "M", img: './img/monje.jpg'},
+    {id: 11, nombre: "Gente tóxica", tipo: "remera", cantidad: 1,desc: "Libro", precio: 700, talle: "S", img: './img/toxica.jpg'},
+    {id: 12, nombre: "Atrévete a ser tu maestro", tipo: "remera", cantidad: 1, desc: "Libro", precio: 700, talle: "S", img: './img/maestro.jpg'},
+]
